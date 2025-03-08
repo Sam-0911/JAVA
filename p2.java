841e256354807d44a8ce50e4f1dc8173a972b897
Sam-0911 <samarthchougule417@gmail.com>
2025-03-08 09:53:29 +0530

    Welcome to C.B.Kore chikodi

--- p2.java
+++ p2.java
@@ -2,6 +2,6 @@ public class p2 {
 
 	public static void main(String[] args) {
 		//TODO Auto-generated method stub
-		System.out.println("Welcome to C.B.Kore");
+		System.out.println("Welcome to C.B.Kore chikodi");
 	}
 }
