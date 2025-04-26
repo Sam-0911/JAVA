b5fa8c8e4171632f3d348644687e76cd3914aaed
Sam-0911 <samarthchougule417@gmail.com>
2025-04-26 17:41:33 +0530

    p4.java

--- p4.java
+++ p4.java
@@ -1,6 +1,6 @@
 public class p4{
-    public static void main(String[]args){
-        TODO Auto-generated method stub
-        System.out.println("Hi how are you")
+    public static void main(String[]args)
+    {
+        System.out.println("Hi how are you");
     }
 }
